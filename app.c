2ad3417d68f1a9be6fa053ae0b76fc9878c1e6c6
Nathan Herrington <Nathanmanforever@gmail.com>
2023-04-21 00:18:57 -0600

    yeet

--- app.c
+++ app.c
@@ -74,6 +74,154 @@ void  physicsTimerCallback (void  *p_tmr, void  *p_arg)
     (void)&p_tmr;
     RTOS_ERR err;
     OSSemPost(&physicsSem, OS_OPT_POST_ALL, &err);
+    while (err.Code != RTOS_ERR_NONE) {}
+}
+#define  PHYSICS_TASK_PRIO            21u  /*   Task Priority.                 */
+#define  PHYSICS_TASK_STK_SIZE       256u  /*   Stack size in CPU_STK.         */
+OS_TCB   physicsTaskTCB;                            /*   Task Control Block.   */
+CPU_STK  physicsTaskStk[PHYSICS_TASK_STK_SIZE]; /*   Stack.  */
+void  physicsTask (void  *p_arg);
+/***************************************************************************//**
+ * @brief
+ *   Creates the idle task.
+ ******************************************************************************/
+void  physicsTaskCreate (void)
+{
+    RTOS_ERR     err;
+
+    OSTaskCreate(&physicsTaskTCB,                /* Pointer to the task's TCB.  */
+                 "Physics Task.",                    /* Name to help debugging.     */
+                 &physicsTask,                   /* Pointer to the task's code. */
+                  DEF_NULL,                          /* Pointer to task's argument. */
+                  PHYSICS_TASK_PRIO,             /* Task's priority.            */
+                 &physicsTaskStk[0],             /* Pointer to base of stack.   */
+                 (PHYSICS_TASK_STK_SIZE / 10u),  /* Stack limit, from base.     */
+                  PHYSICS_TASK_STK_SIZE,         /* Stack size, in CPU_STK.     */
+                  10u,                               /* Messages in task queue.     */
+                  0u,                                /* Round-Robin time quanta.    */
+                  DEF_NULL,                          /* External TCB data.          */
+                  OS_OPT_TASK_STK_CHK,               /* Task options.               */
+                 &err);
+    if (err.Code != RTOS_ERR_NONE) {
+        /* Handle error on task creation. */
+    }
+}
+/***************************************************************************//**
+ * @brief
+ *   The idle task. Enters energy mode 1
+ ******************************************************************************/
+enum objectType {empty, player, satchel, shot};
+struct physicsData {
+    int objectType;
+    int x;
+    int y;
+    int xVel;
+    int yVel;
+    int xAcc;
+    int yAcc;
+    int xForce;
+    int yForce;
+    int xMass;
+    int yMass;
+}
+int playerStartX = 0;
+int playerStartY = 0;
+int rightForce = 10;
+int leftForce = -10;
+int satchelWeight = 10;
+int shotWeight = 1;
+int gravity = -10;
+int playerWeight = 10;
+int maxCapacity = 10;
+struct physicsData[10];
+void  physicsTask (void  *p_arg)
+{
+    /* Use argument. */
+   (void)&p_arg;
+   RTOS_ERR     err;
+    OSTmrStart(&physicsTimer, &err);
+    while (err.Code != RTOS_ERR_NONE) {}
+    for (int i = 0; i < 10; i++) {
+        physicsData[i].objectType = empty;
+        physicsData[i].x = 0;
+        physicsData[i].y = 0;
+        physicsData[i].xVel = 0;
+        physicsData[i].yVel = 0;
+        physicsData[i].xAcc = 0;
+        physicsData[i].yAcc = 0;
+        physicsData[i].xForce = 0;
+        physicsData[i].yForce = 0;
+        physicsData[i].xMass = 0;
+        physicsData[i].yMass = 0;
+    }
+    physicsData[0].objectType = player;
+    physicsData[0].x = playerStartX;
+    physicsData[0].y = playerStartY;
+    int capacity = 10;
+    bool charging = false;
+   while (DEF_TRUE) {
+       OSSemPend(&physicsSem, 0, OS_OPT_PEND_BLOCKING, DEF_NULL, &err);
+       while (err.Code != RTOS_ERR_NONE) {}
+         for (int i = 0; i < 10; i++) {
+                OSMutexPend(&physicsMutex, 0, OS_OPT_PEND_BLOCKING, DEF_NULL, &err);
+                while (err.Code != RTOS_ERR_NONE) {}
+                if ((sliderState.farLeft || sliderState.left) && (sliderState.farRight || sliderState.right)) {
+                    physicsData[i].xForce = 0;
+                } else if (sliderState.farLeft == 1) {
+                    physicsData[i].xForce = leftForce;
+                } else if (sliderState.left == 1) {
+                    physicsData[i].xForce = leftForce / 2;
+                } else if (sliderState.right == 1) {
+                    physicsData[i].xForce = rightForce / 2;
+                } else if (sliderState.farRight == 1) {
+                    physicsData[i].xForce = rightForce;
+                } else {
+                    physicsData[i].xForce = 0;
+                }
+                OSMutexPost(&physicsMutex, OS_OPT_POST_NONE, &err);
+                while (err.Code != RTOS_ERR_NONE) {}
+                // Button 0 is held down, charge capacitor
+                if (buttonStates.button0 == 1) {
+                    charging = true;
+                } else if (buttonStates.button0 == 0 && charging == true) {
+                    charging = false;
+                    if (capacity > 0) {
+                        for (int j = 0; j < 10; j++) {
+                            if (physicsData[j].objectType == empty) {
+                                physicsData[j].objectType = shot;
+                                physicsData[j].x = physicsData[i].x;
+                                physicsData[j].y = physicsData[i].y;
+                                physicsData[j].xVel = physicsData[i].xVel;
+                                physicsData[j].yVel = physicsData[i].yVel;
+                                physicsData[j].xAcc = physicsData[i].xAcc;
+                                physicsData[j].yAcc = physicsData[i].yAcc;
+                                physicsData[j].xForce = physicsData[i].xForce;
+                                physicsData[j].yForce = physicsData[i].yForce;
+                                physicsData[j].xMass = shotWeight;
+                                physicsData[j].yMass = shotWeight;
+                                capacity = 0;;
+                                break;
+                            }
+                        }
+                    }
+                } 
+                if (charging == true && capacity < maxCapacity) {
+                    capacity++;
+                } else if (charging == true) {
+                    capacity = maxCapacity;
+                } 
+                
+
+                physicsData[i].xAcc = physicsData[i].xForce / physicsData[i].xMass;
+                physicsData[i].yAcc = physicsData[i].yForce / physicsData[i].yMass;
+                physicsData[i].xVel += physicsData[i].xAcc;
+                physicsData[i].yVel += physicsData[i].yAcc;
+                physicsData[i].x += physicsData[i].xVel;
+                physicsData[i].y += physicsData[i].yVel;
+         }
+
+   }
+   if (err.Code) {}
 }
 OS_TMR LCDTimer;
 OS_SEM LCDSem;
@@ -210,6 +358,8 @@ void  led0OutputTask (void  *p_arg)
     /* Use argument. */
    (void)&p_arg;
    RTOS_ERR     err;
+   OSTmrStart(&LED0Timer, &err);
+    while (err.Code != RTOS_ERR_NONE) {}
 
     while (DEF_TRUE) {
     }
@@ -302,6 +452,8 @@ void  LCDDisplayTask (void  *p_arg)
     /* Use argument. */
    (void)&p_arg;
    RTOS_ERR     err;
+   OSTmrStart(&LCDTimer, &err);
+    while (err.Code != RTOS_ERR_NONE) {}
     while (DEF_TRUE) {
 
     }
@@ -581,22 +733,17 @@ void app_init(void)
   while (err.Code != RTOS_ERR_NONE) {}
   OSSemCreate(&LCDSem, "LCD Semaphore", 0, &err);
   while (err.Code != RTOS_ERR_NONE) {}
+  // Task Creation
   idleTaskCreate();
   sliderTaskCreate();
    LCDTaskCreate();
    LED0OutputTaskCreate();
    LED1OutputTaskCreate();
   buttonTaskCreate();
+  physicsTaskCreate();
 
+    // Start the OS
   OSStart(&err);
   while (err.Code != RTOS_ERR_NONE) {}
-  OSTmrStart(&LED0Timer, &err);
-  while (err.Code != RTOS_ERR_NONE) {}
-    OSTmrStart(&LCDTimer, &err);
-    while (err.Code != RTOS_ERR_NONE) {}
-    OSTmrStart(&physicsTimer, &err);
-    while (err.Code != RTOS_ERR_NONE) {}
-    OSTmrStart(&sliderTimer, &err);
-    while (err.Code != RTOS_ERR_NONE) {}
 
 }
